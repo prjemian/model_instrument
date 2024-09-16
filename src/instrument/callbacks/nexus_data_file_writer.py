60e395d69a507265acc4b52e9c1043b3f5cfd152
Pete R Jemian <prjemian@gmail.com>
2024-09-16 12:09:15 -0500

    MNT choose NXWriter if at the APS

--- src/instrument/callbacks/nexus_data_file_writer.py
+++ src/instrument/callbacks/nexus_data_file_writer.py
@@ -6,14 +6,17 @@ __all__ = ["nxwriter"]
 
 import logging
 
-# TODO: Choose NXWriter or NXWriterAPS
-from apstools.callbacks import NXWriter
-
 logger = logging.getLogger(__name__)
 logger.info(__file__)
 
 from ..utils.config import iconfig  # noqa
+from ..utils.functions import host_on_aps_subnet  # noqa
 from ..utils.run_engine import RE  # noqa
+
+if host_on_aps_subnet():
+    from apstools.callbacks import NXWriterAPS as NXWriter
+else:
+    from apstools.callbacks import NXWriter
 
 
 class MyNXWriter(NXWriter):
@@ -36,6 +39,6 @@ nxwriter = MyNXWriter()  # create the callback instance
 
 RE.subscribe(nxwriter.receiver)  # subscribe to the RunEngine
 
+nxwriter.file_extension = iconfig.get("FILE_EXTENSION", "hdf")
 warn_missing = iconfig.get("WARN_MISSING", False)
 nxwriter.warn_on_missing_content = warn_missing
-nxwriter.file_extension = iconfig.get("FILE_EXTENSION", "hdf")
--- src/instrument/utils/functions.py
+++ src/instrument/utils/functions.py
@@ -1,18 +1,42 @@
 """
-Utility functions, provides ``running_in_queueserver()``
-========================================================
+Utility functions
+=================
 
 .. autosummary::
 
+    ~host_on_aps_subnet
     ~running_in_queueserver
 """
 
-__all__ = ["running_in_queueserver"]
+__all__ = """
+    host_on_aps_subnet
+    running_in_queueserver
+""".split()
 
 import logging
+import socket
 
 logger = logging.getLogger(__name__)
 logger.info(__file__)
+
+
+def host_on_aps_subnet():
+    """Detect if this host is on an APS subnet."""
+    LOOPBACK_IP4 = "127.0.0.1"
+    PUBLIC_IP4_PREFIX = "164.54."
+    PRIVATE_IP4_PREFIX = "10.54."
+    TEST_IP = "10.254.254.254"  # does not have to be reachable
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.settimeout(0)
+        try:
+            sock.connect((TEST_IP, 1))
+            ip4 = sock.getsockname()[0]
+        except Exception:
+            ip4 = LOOPBACK_IP4
+    return True in [
+        ip4.startswith(PUBLIC_IP4_PREFIX),
+        ip4.startswith(PRIVATE_IP4_PREFIX),
+    ]
 
 
 def running_in_queueserver():
