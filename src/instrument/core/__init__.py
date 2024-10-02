2fb0a857d1753831f6b93d97a4a15d6ee877f300
Pete R Jemian <prjemian@gmail.com>
2024-10-02 18:49:34 -0500

    MNT #3 names were changed

--- src/instrument/core/__init__.py
+++ src/instrument/core/__init__.py
@@ -5,9 +5,9 @@ Also contains setup code that MUST run before other code in this directory.
 """
 
 from ..configs.loaders import iconfig
-from ..utils.aps_helper import aps_dm_setup
-from ..utils.helper import debug_python
-from ..utils.helper import mpl_setup
+from ..utils.aps_helper_functions import aps_dm_setup
+from ..utils.helper_functions import debug_python
+from ..utils.helper_functions import mpl_setup
 
 debug_python()
 mpl_setup()
